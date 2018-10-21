f2f67a850f19ebcf6f10d910728299dec8650610
HeshamAbood <hshm_aboud@yahoo.com>
2018-10-21 23:00:33 +0300

    use mapping for button slots

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -22,7 +22,6 @@ sp=20
 
 
 def win_check(board, mark):
-
     return ((board[7] == mark and board[8] == mark and board[9] == mark) or  
             (board[4] == mark and board[5] == mark and board[6] == mark) or  
             (board[1] == mark and board[2] == mark and board[3] == mark) or  
@@ -31,7 +30,6 @@ def win_check(board, mark):
             (board[9] == mark and board[6] == mark and board[3] == mark) or  
             (board[7] == mark and board[5] == mark and board[3] == mark) or  
             (board[9] == mark and board[5] == mark and board[1] == mark))
-
 
 
 test_board = ['#',None,None,None,None,None,None,None,None,None]
@@ -56,7 +54,6 @@ print(player_mark)
 def on_button(n,nn):
     print('Button {} clicked {}'.format(n,nn))
     global player_mark
-    print("button clicked")
     print("playermark", player_mark)
     mark=player_mark
     
@@ -71,7 +68,6 @@ def on_button(n,nn):
             print("player1 win")
         else:
             print ("player2 win")
-
 
     if player_mark=="x":
         player_mark="o"
@@ -89,17 +85,24 @@ for r in play_btn:
         i+=1
         btn.setIcon(QIcon(pixmap))
         #btn.clicked.connect(toggleBtnPic)
-        btn.clicked.connect(lambda: on_button(btn,i))
-
-
+        #btn.clicked.connect(lambda btn=btn,i=i : on_button(btn,i))
     wid=100
     hgh+=100+sp
     print(rdx)
     rdx+=1
-    
+
+mapping = [(play_btn[0][0], 1),
+           (play_btn[0][1], 2),
+           (play_btn[0][2], 3),
+           (play_btn[1][0], 4),
+           (play_btn[1][1], 5),
+           (play_btn[1][2], 6),
+           (play_btn[2][0], 7),
+           (play_btn[2][1], 8),
+           (play_btn[2][2], 9)]
+
+for button, n in mapping:
+    button.clicked.connect(lambda  state, n=n: on_button(button,n))
 
 window.show()
 app.exec_()
-
-
-
